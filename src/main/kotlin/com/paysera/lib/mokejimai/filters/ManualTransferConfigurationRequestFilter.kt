ac567f1a6439feb6060ee9f29af5d17faf82d3b7
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-06-18 10:56:33 +0300

    Don't create moshi adapter for filters

--- src/main/kotlin/com/paysera/lib/mokejimai/filters/ManualTransferConfigurationRequestFilter.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/filters/ManualTransferConfigurationRequestFilter.kt
@@ -1,10 +1,7 @@
 package com.paysera.lib.mokejimai.filters
 
 import com.paysera.lib.common.entities.BaseFilter
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
 class ManualTransferConfigurationRequestFilter(
     offset: Int? = null,
     limit: Int? = null,
@@ -12,16 +9,11 @@ class ManualTransferConfigurationRequestFilter(
     orderDirection: String? = null,
     after: String? = null,
     before: String? = null,
-    @Json(name = "from_bank_key")
     val fromBankKey: String? = null,
-    @Json(name = "from_country_code")
     val fromCountryCode: String? = null,
     val currency: String? = null,
-    @Json(name = "to_bank_key")
     val toBankKey: String? = null,
-    @Json(name = "to_country_code")
     val toCountryCode: String? = null,
-    @Json(name = "to_iban")
     val toIban: String? = null,
     val locale: String? = null
 ) : BaseFilter(
