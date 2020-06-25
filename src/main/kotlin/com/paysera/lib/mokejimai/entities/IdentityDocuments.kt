f2c13efbca7d7c63a1b2a33939e49687d263249c
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-06-25 09:59:10 +0300

    merge with upstream

--- src/main/kotlin/com/paysera/lib/mokejimai/entities/IdentityDocuments.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/entities/IdentityDocuments.kt
@@ -1,6 +1,11 @@
 package com.paysera.lib.mokejimai.entities
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class IdentityDocuments(
+    @Json(name = "document_type")
     var documentType: String,
     var country: String,
     var type: String,
